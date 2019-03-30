4aa6353abcc0d350828f7ae6d610cb3f813abdb2
kailunyao <yaokailun@ifanr.com>
2019-03-30 23:42:46 +0800

    Update notes-data.js

--- js/notes-data.js
+++ js/notes-data.js
@@ -4,4 +4,9 @@ window.notesData = [
     link: 'bianma.md',
     date: '2019/02/06',
   },
+  {
+    name: '读书笔记：企业 IT 架构之道：阿里巴巴中台战略思想与架构实战',
+    link: 'qi-ye-it-jia-gou-zhi-dao.md',
+    date: '2019/03/30',
+  }
 ]
