55001aee2cbb42457ffe764a36056021ce5ca3b3
kailunyao <571091612@qq.com>
2022-12-01 00:05:20 +0800

    Update task-data.js

--- js/task-data.js
+++ js/task-data.js
@@ -133,6 +133,11 @@ window.readedBooks = [
     start: '2022/06',
   },
   {
+    name: '法治的细节',
+    start: '2022/11',
+    end: '2022/11',
+  },
+  {
     name: '可能性的艺术',
     start: '2022/11',
     end: '2022/11',
